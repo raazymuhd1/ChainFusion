9d7e90d07520aabef5fba265b9c3c2e4f82425ec
raazymuhd1 <muhammadrazi51@gmail.com>
2025-06-21 20:07:56 +0700

    added a connectWallet button on MainHeader

--- src/components/mainHeader/MainHeader.tsx
+++ src/components/mainHeader/MainHeader.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+"use client"
+import { useState } from 'react'
+import { CustomButton } from "@/components"
 
 const MainHeader = () => {
+    const [showAddress, setShowAddress] = useState(false)
+
   return (
-    <header className='w-[80%] mx-auto h-[80px] sticky top-[10px] p-[15px] glass_bg rounded-[0] z-[2] shadows_orange'>
-        <h2> Main Header </h2>
+    <header className='w-[80%] mx-auto h-[80px] sticky top-[10px] p-[15px] glass_bg rounded-[0] z-[2] shadows_orange flex items-center justify-end gap-[20px] shadows'>
+       <CustomButton title={`Connect Wallet`} styles='bg-[var(--warning)] p-[10px]' />
+       {/* user wallet */}
+       <div className="flex items-center gap-[20px]">
+          <p className={`${!showAddress && "hidden"} `}> user address </p>
+          {/* copyable */}
+       </div>
     </header>
   )
 }
